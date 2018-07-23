d3be83e1e9a201181a673f45f307e5bcd699c23f
Gabriel Mutua <GabuTheGreat@users.noreply.github.com>
2018-07-23 10:42:42 +0300

    Update server.R

--- Sentiment_Analysis/server.R
+++ Sentiment_Analysis/server.R
@@ -16,10 +16,11 @@ library(sqldf)
 
 
 #Connecting R to Twitter
-consumerKey <- "QffMSiv8lBcSijEd3KcS3AaA7" 
-consumerSecret <- "eEn1rhFJRbX3fzhJcTyrblCtuglFzUUqBkeKwG6KRMe2sM6jqR"
-accessToken <- "4766523441-9fK3r9Nib3QEfc7ZPABfvVfPUI8ghlBz8hAOiP4"
-accessTokenSecret <- "nnU5lNwZwttEhsWcp2npIIYjD5apPjyQozxDBSzDCNRQu"
+#Update consumerKey, consumerSecret, accessToken, accessTokenSecret using your app details. 
+consumerKey <- "XXXXXXXXXXXXXXXX" 
+consumerSecret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+accessToken <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+accessTokenSecret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 options(httr_oauth_cache=T)
 setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessTokenSecret)
 
